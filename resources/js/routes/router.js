62ced15744e67d3781c9c696800dd48f2c65ecfc
roni599 <ekroni99@gmail.com>
2026-04-27 00:37:05 +0600

    veryfy otp

--- resources/js/routes/router.js
+++ resources/js/routes/router.js
@@ -1,50 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/authStore'
 
-import Login from '@/components/Login.vue'
-import Register from '@/components/Register.vue'
-import Forget from '@/components/Forget.vue'
-import VerifyOtp from '@/components/VerifyOtp.vue'
-import Home from '@/components/Home.vue'
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
+import Forget from '../components/Forget.vue'
+import VeryfyOtp from '../components/VeryfyOtp.vue'
+import Home from '../components/Home.vue'
 
 const routes = [
-    { name: 'Login', path: '/', component: Login, meta: { guest: true } },
-    { name: 'Register', path: '/register', component: Register, meta: { guest: true } },
-    { name: 'Forget', path: '/forget', component: Forget, meta: { guest: true } },
-    { name: 'VerifyOtp', path: '/verify-otp', component: VerifyOtp, meta: { guest: true, otp: true } },
-    { name: 'Home', path: '/home', component: Home, meta: { requiresAuth: true } },
-    { path: '/:pathMatch(.*)*', redirect: '/' }
+    {
+        name: 'LoginForm',
+        component: Login,
+        path: '/',
+        meta: { guest: true }
+    },
+    {
+        name: 'RegisterForm',
+        component: Register,
+        path: '/register',
+        meta: { guest: true }
+    },
+    {
+        name: 'Forget',
+        component: Forget,
+        path: '/forget',
+        meta: { guest: true }
+    },
+    {
+        name: 'VeryfyOtp',
+        component: VeryfyOtp,
+        path: '/verify-otp',
+        meta: { guest: true }
+    },
+    {
+        name: 'Home',
+        component: Home,
+        path: '/home',
+        meta: { requiresAuth: true }
+    },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
-function getAuth() {
-    try {
-        return JSON.parse(localStorage.getItem('auth-store')) || {}
-    } catch (e) {
-        return {}
-    }
-}
 
 router.beforeEach((to, from, next) => {
-    const auth = getAuth()
-    const token = auth.token
-    const isLoggedIn = !!token
-
-    if (to.meta.requiresAuth && !isLoggedIn) {
-        return next({ name: 'Login' })
+    const auth = useAuthStore()
+    if (to.meta.requiresAuth && !auth.isAuthenticated) {
+        return next('/')
     }
-    if (to.meta.guest && isLoggedIn) {
-        return next({ name: 'Home' })
-    }
-    if (to.meta.otp) {
-
+    if (to.path === '/verify-otp') {
         const email = sessionStorage.getItem('reset_email')
-
         if (!email || !auth.otpSent) {
-            return next({ name: 'Login' })
+            return next('/')
         }
+    }
+    if (to.meta.guest && auth.isAuthenticated) {
+        return next('/home')
     }
     next()
 })
